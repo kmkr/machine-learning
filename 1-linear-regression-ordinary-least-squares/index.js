630b363b0d6b44408dfb769e2565169dd4127ad1
Kris-Mikael Krister <krismikael@protonmail.com>
2017-08-23 21:44:08 +0200

    Add lr with ordinary least squares

---
+++ 1-linear-regression-ordinary-least-squares/index.js
@@ -0,0 +1,68 @@
+const data = [];
+
+let m = 1;
+let b = 0;
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function linearRegression() {
+    const {x:xsum, y:ysum} = data.reduce((prevVal, curVal) => {
+        prevVal.x += curVal.x;
+        prevVal.y += curVal.x;
+        return prevVal;
+    }, { x: 0, y: 0 });
+
+    const xmean = xsum / data.length;
+    const ymean = ysum / data.length;
+
+    let numerator = 0;
+    let denominator = 0;
+
+    data.forEach(({x, y}) => {
+        numerator += (x - xmean) * (y - ymean);
+        denominator += (x - xmean) * (x - ymean);
+    });
+
+    m = numerator / denominator;
+    b = ymean - m * xmean;
+}
+
+function drawLine() {
+    const x1 = 0;
+    const y1 = m * x1 + b;
+    const x2 = 1;
+    const y2 = m * x2 +b;
+    stroke(255, 0, 255);
+
+    const mx1 = map(x1, 0, 1, 0, width);
+    const my1 = map(y1, 0, 1, height, 0);
+    const mx2 = map(x2, 0, 1, 0, width);
+    const my2 = map(y2, 0, 1, height, 0);
+    line(mx1, my1, mx2, my2);
+}
+
+function mousePressed() {
+    const x = map(mouseX, 0, width, 0, 1);
+    const y = map(mouseY, 0, height, 1, 0);
+    
+    const point = createVector(x, y);
+    data.push(point)
+}
+
+function draw() {
+    background(51);    
+    data.forEach(elem => {
+        const x = map(elem.x, 0, 1, 0, width);
+        const y = map(elem.y, 0, 1, height, 0);
+        fill(255);
+        stroke(255);
+        ellipse(x, y, 8, 8);
+    });
+
+    if (data.length > 1) {
+        linearRegression();
+        drawLine();
+    }
+}
