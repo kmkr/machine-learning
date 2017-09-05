2e832c5e54004fa391a48976d8187c2ce62d5b9d
Kris-Mikael Krister <krismikael@protonmail.com>
2017-09-05 12:23:09 +0200

    Add tests

--- inf4490/task_2/pmx.py
+++ inf4490/task_2/pmx.py
@@ -1,6 +1,6 @@
 import random
 import math
-# 
+#
 # Given the sequences (2,4,7,1,3,6,8,9,5) and (5,9,8,6,2,4,1,3,7).
 # Implement these algorithms to create a new pair of solutions:
 # \renewcommand{\theenumi}{\alph{enumi}}
@@ -10,26 +10,24 @@ import math
 def partial_mapped_crossover(P1, P2):
     cross_a = random.randint(0, len(P1) - 2)
     cross_b = random.randint(cross_a, len(P1) - 1)
-    
-    print('Cross A: ' + str(cross_a) + ' Cross B : ' + str(cross_b))
+
+    print 'Cross A: ' + str(cross_a) + ' Cross B : ' + str(cross_b)
 
     child_1 = child_from(P1, P2, cross_a, cross_b)
     child_2 = child_from(P2, P1, cross_a, cross_b)
 
-def child_from(P1, P2, cross_a, cross_b):
-    print('P1: ' + ', '.join(str(e) for e in P1))
-    print('P2: ' + ', '.join(str(e) for e in P2))
-    child = []
-    for x in range(len(P1)):
-        if x >= cross_a and x <= cross_b:
-            # I segment
-            child.append(P1[x])
-        else:
-            # Utenfor segment
-            child.append(-1)
+    return child_1, child_2
 
-    print('Child etter step 1 (segment copy):')
-    print(child)
+def child_from(P1, P2, cross_a, cross_b):
+    print 'P1: ' + ', '.join(str(e) for e in P1)
+    print 'P2: ' + ', '.join(str(e) for e in P2)
+    child = [None]*len(P1)
+
+    for x in range(cross_a, cross_b + 1):
+        child[x] = P1[x]
+
+    print 'Child etter step 1 (segment copy):'
+    print child
 
     for x in range(cross_a - 1, cross_b):
         p2_index = cross_a
@@ -43,27 +41,27 @@ def child_from(P1, P2, cross_a, cross_b):
             index = p2_index
             if p1_segment_val == p2_segment_val:
                 break
-            
+
         if not p2_segment_val in child:
             child[p2_index] = p2_segment_val
 
-        print(child)
+        print child
 
-    print('Child etter step 2:')
-    print(child)
+    print 'Child etter step 2:'
+    print child
 
     # Kopier resterende verdier fra P2:
-    for x in range(len(child)):
-        if (child[x] == -1):
-            child[x] = P2[x]
+    for i, item in enumerate(child):
+        if item is None:
+            child[i] = P2[i]
 
-    print('Child etter step 3:')
-    print(child)
-    print('\n')
+    print 'Child etter step 3:'
+    print child
+    print '\n'
     return child
 
 
-seq_1 = [2,4,7,1,3,6,8,9,5]
-seq_2 = [5,9,8,6,2,4,1,3,7]
+#seq_1 = [2,4,7,1,3,6,8,9,5]
+#seq_2 = [5,9,8,6,2,4,1,3,7]
 
-partial_mapped_crossover(seq_1, seq_2)
+#partial_mapped_crossover(seq_1, seq_2)
