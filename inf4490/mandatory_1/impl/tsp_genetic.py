e71e7c8e6e4a2c2414be65c459073d3d56083148
Kris-Mikael Krister <krismikael@protonmail.com>
2017-09-20 21:12:34 +0200

    Add results

--- inf4490/mandatory_1/impl/tsp_genetic.py
+++ inf4490/mandatory_1/impl/tsp_genetic.py
@@ -7,14 +7,15 @@ import random
 import statistics
 from time import time
 from functools import reduce
+import numpy as np
 import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(dir_path, '..', '..')) # To import modules from task_2 folder
 sys.path.append(os.path.join(dir_path, '..', '..', 'task_2')) # Setting task_2 as path will avoid having to re-write module imports in task_2 files
 import task_2.pmx
 
-NUM_GENERATIONS = 1000
-POPULATION_SIZE = 100
+MAX_GENERATIONS = 100
+MAX_WITHOUT_CHANGE = 10
 NUM_EXECUTIONS = 20
 
 def genetic(num_cities, population_size):
@@ -92,14 +93,11 @@ def generate_offspring(parents, num_offspring):
     return offspring
 
 def get_shortest_route(distance_dataset, population):
-    shortest = { 'distance': float('inf') }
-    for individual in population:
-        route_distance = distance_helper.get_route_distance(distance_dataset, individual)
-        if route_distance < shortest['distance']:
-            shortest = {
-                'distance': route_distance,
-                'individual': individual
-            }
+    shortest = { 'route_distance': float('inf') }
+    evaluated_population = evaluate_population(distance_dataset, population)
+    for individual in evaluated_population:
+        if individual['route_distance'] < shortest['route_distance']:
+            shortest = individual
 
     return shortest
 
@@ -107,7 +105,9 @@ def select_survivors(distance_dataset, parents, offspring, population_size):
     # Discard parents ((μ,λ) selection) and select best-fit offspring
     evaluated_offspring = evaluate_population(distance_dataset, offspring)
     sorted_offspring = sorted(evaluated_offspring, key=lambda offspring: offspring['route_distance'])
-    return map((lambda x: x['route']), sorted_offspring[0:population_size])
+    survivors = list(map((lambda x: x['route']), sorted_offspring[0:population_size]))
+
+    return survivors, sorted_offspring[0]
 
 def find_shortest_path_for_cities(distance_dataset, num_cities, population_size):
     start = time()
@@ -115,47 +115,67 @@ def find_shortest_path_for_cities(distance_dataset, num_cities, population_size)
     cities = list(distance_dataset[0][0:num_cities])
     population = generate_population(cities, population_size)
     num_generation = 0
+    shortest_per_generation = []
 
-    while num_generation < NUM_GENERATIONS:
+    while num_generation < MAX_GENERATIONS:
         evaluated_population = evaluate_population(distance_dataset, population)
         num_parents = population_size / 2
         parents = stochastic_universal_sampling(evaluated_population, num_parents)
         num_offspring = population_size * 2
         offspring = generate_offspring(parents, num_offspring)
-        population = select_survivors(distance_dataset, parents, offspring, population_size)
+        population, shortest = select_survivors(distance_dataset, parents, offspring, population_size)
+        shortest_per_generation.append(shortest['route_distance'])
         num_generation = num_generation + 1
 
     shortest = get_shortest_route(distance_dataset, population)
     end = time()
-    print(distance_helper.get_route_distance(distance_dataset, shortest['individual']))
-    return shortest, end-start
+    return shortest, end-start, shortest_per_generation
 
-def run(num_cities):
+def run(num_cities, population_size):
     best = float('inf')
     best_route = None
     worst = 0
     distances = []
+    total_duration = 0
 
-    print('Genetic algorithm using first ' + str(num_cities) + ' cities')
-    for _ in range(NUM_EXECUTIONS):
-        shortest, duration = genetic(num_cities, POPULATION_SIZE)
-        if shortest['distance'] < best:
-            best = shortest['distance']
-            best_route = shortest['individual']
-        if shortest['distance'] > worst:
-            worst = shortest['distance']
+    print('Genetic algorithm using population size ' + str(population_size) + ' with ' + str(MAX_GENERATIONS) + ' generations on first ' + str(num_cities) + ' cities ')
+    for num_execution in range(NUM_EXECUTIONS):
+        shortest, duration, shortest_per_generation = genetic(num_cities, population_size)
+        print('Execution ' + str(num_execution) + ': ' + str(shortest['route_distance']) + ' km (' + str(duration) + ' s)')
+        if shortest['route_distance'] < best:
+            best = shortest['route_distance']
+            best_route = shortest['route']
+        if shortest['route_distance'] > worst:
+            worst = shortest['route_distance']
 
-        distances.append(shortest['distance'])
+        distances.append(shortest['route_distance'])
+        total_duration = total_duration + duration
 
-    print('Best   ' + str(best))
+    print('Best     ' + str(best))
     print(best_route)
-    print('Worst  ' + str(worst))
-    print('Mean   ' + str(sum(distances) / NUM_EXECUTIONS))
-    print('stdev  ' + str(statistics.stdev(distances)))
+    print('Worst    ' + str(worst))
+    print('Mean     ' + str(sum(distances) / NUM_EXECUTIONS))
+    print('stdev    ' + str(statistics.stdev(distances)))
+    print('duration ' + str(total_duration))
+    print('\n\n')
 
+    return shortest_per_generation
+
+
+def run_three_variants(num_cities):
+    shortest_per_generation_50 = run(num_cities, 50)
+    shortest_per_generation_100 = run(num_cities, 100)
+    shortest_per_generation_200 = run(num_cities, 200)
+
+    shortest_per_generation_average = np.mean(np.array([shortest_per_generation_50, shortest_per_generation_100, shortest_per_generation_200]), axis=0)
+    print('Pop 50')
+    print(shortest_per_generation_50)
+    print('Pop 100')
+    print(shortest_per_generation_100)
+    print('Pop 200')
+    print(shortest_per_generation_200)
+    print('Avg')
+    print(shortest_per_generation_average)
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        run(int(sys.argv[1]))
-    else:
-        run(5)
+    run_three_variants(int(sys.argv[1]) if len(sys.argv) > 1 else 5)
