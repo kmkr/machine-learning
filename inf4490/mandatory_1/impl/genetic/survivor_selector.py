770b89ea64e82c952d5f0348165a429a704359d7
Kris-Mikael Krister <krismikael@protonmail.com>
2017-09-24 22:39:02 +0200

    Impl baldwian

--- inf4490/mandatory_1/impl/genetic/survivor_selector.py
+++ inf4490/mandatory_1/impl/genetic/survivor_selector.py
@@ -4,12 +4,24 @@ from hill_climber.hill_climb import hill_climb
 
 MAX_ITERATIONS = 10
 
+def mapper(distance_dataset, evaluated_individual):
+    result = hill_climb(distance_dataset, evaluated_individual['route'], MAX_ITERATIONS)
+    return {
+        'orig': evaluated_individual['route'],
+        'route_distance': result['route_distance'],
+        'route': result['route'],
+    }
+
 def _improve_with_hill_climb(distance_dataset, evaluated_offspring):
-    return list(map(lambda evaluated: hill_climb(distance_dataset, evaluated['route'], MAX_ITERATIONS), evaluated_offspring))
+    return list(map(lambda x: mapper(distance_dataset, x), evaluated_offspring))
 
 def _sort_by_distance(evaluated_offspring):
     return sorted(evaluated_offspring, key=lambda offspring: offspring['route_distance'])
 
+def _get_survivors(population_size, individual_key, sorted_offspring):
+    return list(map((lambda individual: individual[individual_key]), sorted_offspring[0:population_size]))
+
+# Discard parents ((μ,λ) selection) and select best-fit offspring
 def select_survivors(distance_dataset, parents, offspring, population_size, survivor_selector_strategy):
     evaluated_offspring = population_evaluator.evaluate_population(distance_dataset, offspring)
 
@@ -18,15 +30,16 @@ def select_survivors(distance_dataset, parents, offspring, population_size, surv
         hill_climbed_offspring = _improve_with_hill_climb(distance_dataset, evaluated_offspring)
 
         evaluated_offspring = hill_climbed_offspring
+        sorted_offspring = _sort_by_distance(evaluated_offspring)
+        survivors = _get_survivors(population_size, 'route', sorted_offspring)
     elif survivor_selector_strategy == SurvivorStrategy.HYBRID_HILL_BALDWINIAN:
         ## hill climb, bruk resultatet for selection, men unngå å endre offspring
         hill_climbed_offspring = _improve_with_hill_climb(distance_dataset, evaluated_offspring)
         sorted_hill_climbed_offspring = _sort_by_distance(hill_climbed_offspring)
-        # ????
-        print(2)
-
-    # Discard parents ((μ,λ) selection) and select best-fit offspring
-    sorted_offspring = _sort_by_distance(evaluated_offspring)
-    survivors = list(map((lambda x: x['route']), sorted_offspring[0:population_size]))
+        sorted_offspring = _sort_by_distance(evaluated_offspring)
+        survivors = _get_survivors(population_size, 'orig', sorted_hill_climbed_offspring)
+    else:
+        sorted_offspring = _sort_by_distance(evaluated_offspring)
+        survivors = _get_survivors(population_size, 'route', sorted_offspring)
 
     return survivors, sorted_offspring[0]
---
+++ inf4490/mandatory_1/impl/tsp_genetic_baldwinian.py
@@ -0,0 +1,15 @@
+import sys
+import genetic.genetic
+from genetic.survivor_strategy import SurvivorStrategy
+
+if __name__ == '__main__':
+    num_cities = int(sys.argv[1]) if len(sys.argv) > 1 else 5
+    results = genetic.genetic.run_three_variants(num_cities, SurvivorStrategy.HYBRID_HILL_BALDWINIAN)
+    print('Pop 50')
+    print(results['50'])
+    print('\nPop 100')
+    print(results['100'])
+    print('\nPop 200')
+    print(results['200'])
+    print('\nAvg')
+    print(results['avg'])
